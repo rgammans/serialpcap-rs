1d81d086595aa796c3ab09558ddd86ac5463e39b
Roger Gammans <rgammans@gammascience.co.uk>
2025-07-06 13:08:58 +0100

    Simpoify delagtion with Deref..
    potentiall bad practice

--- src/portinfo.rs
+++ src/portinfo.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Write, io::Read};
+use std::ops::{Deref, DerefMut};
 
 use serialport::{self, SerialPort};
 use gpio::{GpioOut, GpioValue};
@@ -60,54 +60,14 @@ where T: serialport::SerialPort
     }
 }
 
-impl<T> std::io::Write for SerialReflectorPort<T>
+impl<T> Deref for SerialReflectorPort<T>
 where T: serialport::SerialPort
 {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        self.port.write(buf)
-    }
+    type Target = T;
 
-    fn flush(&mut self) -> std::io::Result<()> {
-        self.port.flush()
+    fn deref(&self) -> &Self::Target {
+        &self.port
     }
-}   
-
-impl<T> std::io::Read for SerialReflectorPort<T>
-where T: serialport::SerialPort
-{
-    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        self.port.read(buf)
-    }
-}   
-
-impl<T> SerialPort for SerialReflectorPort<T> 
-where T: serialport::SerialPort //+ std::io::Write + std::io::Read
-{
-    fn name(&self) -> Option<String> { self.port.name() }
-    fn baud_rate(&self) -> serialport::Result<u32> { self.port.baud_rate() }
-    fn data_bits(&self) -> Result<serialport::DataBits, serialport::Error> { self.port.data_bits() }
-    fn flow_control(&self) -> Result<serialport::FlowControl, serialport::Error> { self.port.flow_control() }
-    fn parity(&self) -> Result<serialport::Parity, serialport::Error> { self.port.parity() }
-    fn stop_bits(&self) -> Result<serialport::StopBits, serialport::Error> { self.port.stop_bits() }
-    fn timeout(&self) -> std::time::Duration { self.port.timeout() }
-    fn write_request_to_send(&mut self, level: bool) -> serialport::Result<()> { self.port.write_request_to_send(level) } 
-    fn write_data_terminal_ready(&mut self, level: bool) -> serialport::Result<()> { self.port.write_data_terminal_ready(level) }
-    fn read_clear_to_send(&mut self) -> serialport::Result<bool> { self.port.read_clear_to_send() }
-    fn read_data_set_ready(&mut self) -> serialport::Result<bool> { self.port.read_data_set_ready() }
-    fn read_ring_indicator(&mut self) -> serialport::Result<bool> { self.port.read_ring_indicator() }
-    fn read_carrier_detect(&mut self) -> serialport::Result<bool> { self.port.read_carrier_detect() }
-    fn bytes_to_read(&self) -> serialport::Result<u32> { self.port.bytes_to_read() }
-    fn bytes_to_write(&self) -> serialport::Result<u32> { self.port.bytes_to_write() }
-    fn clear(&self, buffer_to_clear: serialport::ClearBuffer) -> serialport::Result<()> { self.port.clear(buffer_to_clear) }
-    fn try_clone(&self) -> serialport::Result<Box<dyn serialport::SerialPort>> { self.port.try_clone() }
-    fn set_baud_rate(&mut self, baud_rate: u32) -> serialport::Result<()> { self.port.set_baud_rate(baud_rate) }
-    fn set_data_bits(&mut self, data_bits: serialport::DataBits) -> serialport::Result<()> { self.port.set_data_bits(data_bits) }
-    fn set_flow_control(&mut self, flow_control: serialport::FlowControl) -> serialport::Result<()> { self.port.set_flow_control(flow_control) }
-    fn set_parity(&mut self, parity: serialport::Parity) -> serialport::Result<()> { self.port.set_parity(parity) }
-    fn set_stop_bits(&mut self, stop_bits: serialport::StopBits) -> serialport::Result<()> { self.port.set_stop_bits(stop_bits) }
-    fn set_timeout(&mut self, timeout: std::time::Duration) -> serialport::Result<()> { self.port.set_timeout(timeout) }
-    fn set_break(&self) -> serialport::Result<()> { self.port.set_break() }
-    fn clear_break(&self) -> serialport::Result<()> { self.port.clear_break() }
 }
 
 impl<T> SerialReflection for SerialReflectorPort<T>
@@ -171,9 +131,7 @@ where
     T: serialport::SerialPort + AdvancedSerialPort,
 {
     fn reflect_control_lines(&mut self, lines: &PortControlLines) -> serialport::Result<()> {
-         { SerialReflectorPort::new(Box::new(self)).reflect_control_lines(lines)?; }
-        //self.write_data_terminal_ready(lines.dsr)?;
-        //self.write_request_to_send(lines.cts)?;
+        self.reflect_control_lines(lines)?;  // Do the basic reflection
         if self.can_set_ring_indicator() {
             self.set_ring_indicator(lines.ri)?;
         }
@@ -195,7 +153,7 @@ where
     
 }
 
-pub struct SerialPortWrapper<T, G>
+pub struct SerialPortWithGpios<T, G>
 where
     T: serialport::SerialPort,
     G: GpioOut,
@@ -206,13 +164,14 @@ where
     last_set_rts: Option<bool>,
     last_set_dtr: Option<bool>,
 }
-impl<T,G> SerialPortWrapper<T,G>
+
+impl<T,G> SerialPortWithGpios<T,G>
 where
     T: serialport::SerialPort,
     G: GpioOut,
 {
     pub fn new(port: T, ri_out_gpio: Option<G>, cd_out_gpio: Option<G>) -> Self {
-        SerialPortWrapper {
+        SerialPortWithGpios {
             port,
             ri_out_gpio,
             cd_out_gpio,
@@ -220,77 +179,84 @@ where
             last_set_dtr: None,
         }
     }
-
-}   
-
-impl<T,G> std::io::Write for SerialPortWrapper<T,G>
-where 
-    T: serialport::SerialPort,
-    G: GpioOut,
-{
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        self.port.write(buf)
-    }
-
-    fn flush(&mut self) -> std::io::Result<()> {
-        self.port.flush()
-    }   
-}
-
-impl<T,G> Read for SerialPortWrapper<T,G>  
-where 
-    T: serialport::SerialPort,
-    G: GpioOut,
-{
-    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        self.port.read(buf)
-    }
-}
-
-impl<T,G> SerialPort for SerialPortWrapper<T,G>    
-where 
-T: serialport::SerialPort,
-G: GpioOut + Send,
-{
-    // Delegate all methods to inner port
-    fn name(&self) -> Option<String> { self.port.name() }
-    fn baud_rate(&self) -> serialport::Result<u32> { self.port.baud_rate() }
-    fn data_bits(&self) -> Result<serialport::DataBits, serialport::Error> { self.port.data_bits() }
-    fn flow_control(&self) -> Result<serialport::FlowControl, serialport::Error> { self.port.flow_control() }
-    fn parity(&self) -> Result<serialport::Parity, serialport::Error> { self.port.parity() }
-    fn stop_bits(&self) -> Result<serialport::StopBits, serialport::Error> { self.port.stop_bits() }
-    fn timeout(&self) -> std::time::Duration { self.port.timeout() }
-    fn write_request_to_send(&mut self, level: bool) -> serialport::Result<()> { 
-        self.port.write_request_to_send(level).and_then(|_| {
-                self.last_set_rts = Some(level);
-                Ok(())
-            })  
-    }
-    fn write_data_terminal_ready(&mut self, level: bool) -> serialport::Result<()> { 
-        self.port.write_data_terminal_ready(level).and_then(|_| {
-            self.last_set_dtr = Some(level);
+    pub fn write_request_to_send(&mut self, level: bool) -> serialport::Result<()> {
+        self.port.write_request_to_send(level).and_then( |_|{
+            self.last_set_rts = Some(level);
             Ok(())
+        }).or_else(|e| {
+            Err(serialport::Error::new(
+                serialport::ErrorKind::Unknown,
+                format!("Failed to set Request To Send: {}", e),
+            ))
         })
     }
-    fn read_clear_to_send(&mut self) -> serialport::Result<bool> { self.port.read_clear_to_send() }
-    fn read_data_set_ready(&mut self) -> serialport::Result<bool> { self.port.read_data_set_ready() }
-    fn read_ring_indicator(&mut self) -> serialport::Result<bool> { self.port.read_ring_indicator() }
-    fn read_carrier_detect(&mut self) -> serialport::Result<bool> { self.port.read_carrier_detect() }
-    fn bytes_to_read(&self) -> serialport::Result<u32> { self.port.bytes_to_read() }
-    fn bytes_to_write(&self) -> serialport::Result<u32> { self.port.bytes_to_write() }
-    fn clear(&self, buffer_to_clear: serialport::ClearBuffer) -> serialport::Result<()> { self.port.clear(buffer_to_clear) }
-    fn try_clone(&self) -> serialport::Result<Box<dyn serialport::SerialPort>> { self.port.try_clone() }
-    fn set_baud_rate(&mut self, baud_rate: u32) -> serialport::Result<()> { self.port.set_baud_rate(baud_rate) }
-    fn set_data_bits(&mut self, data_bits: serialport::DataBits) -> serialport::Result<()> { self.port.set_data_bits(data_bits) }
-    fn set_flow_control(&mut self, flow_control: serialport::FlowControl) -> serialport::Result<()> { self.port.set_flow_control(flow_control) }
-    fn set_parity(&mut self, parity: serialport::Parity) -> serialport::Result<()> { self.port.set_parity(parity) }
-    fn set_stop_bits(&mut self, stop_bits: serialport::StopBits) -> serialport::Result<()> { self.port.set_stop_bits(stop_bits) }
-    fn set_timeout(&mut self, timeout: std::time::Duration) -> serialport::Result<()> { self.port.set_timeout(timeout) }
-    fn set_break(&self) -> serialport::Result<()> { self.port.set_break() }
-    fn clear_break(&self) -> serialport::Result<()> { self.port.clear_break() }
+    pub fn write_data_terminal_ready(&mut self, level: bool) -> serialport::Result<()> {
+        self.port.write_data_terminal_ready(level).and_then( |_|{
+            self.last_set_dtr = Some(level);
+            Ok(())
+        }).or_else(|e| {
+            Err(serialport::Error::new(
+                serialport::ErrorKind::Unknown,
+                format!("Failed to set Request To Send: {}", e),
+            ))
+        })
+    }
+}   
+
+impl<T,G> Deref for SerialPortWithGpios<T,G>
+where
+    T: serialport::SerialPort,
+    G: GpioOut + Send,
+{
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.port
+    }
 }
 
-impl<T,G> AdvancedSerialPort for SerialPortWrapper<T,G>
+impl<T,G> DerefMut for SerialPortWithGpios<T,G>
+where
+    T: serialport::SerialPort,
+    G: GpioOut + Send,
+{
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.port
+    }
+}
+
+
+
+impl<T,G> SerialReflection for SerialPortWithGpios<T,G>
+where
+    T: serialport::SerialPort,
+    G: GpioOut + Send,
+{
+    fn reflect_control_lines(&mut self, lines: &PortControlLines) -> serialport::Result<()> {
+        //self.port.write_data_set_ready(lines.dsr)?;
+        //self.port.write_clear_to_send(lines.cts)?;
+        self.set_carrier_detect(lines.cd)?;
+        self.set_ring_indicator(lines.ri)?;
+        self.write_request_to_send(lines.rts)?;
+        self.write_data_terminal_ready(lines.dtr)?;
+        Ok(())
+    }
+
+    fn capture_control_lines(&mut self) -> serialport::Result<PortControlLines> {
+        let mut lines = PortControlLines::new();
+        lines.dsr = self.read_data_set_ready()?;
+        lines.cts = self.read_clear_to_send()?;
+        lines.cd = self.read_carrier_detect()?;
+        lines.ri = self.read_ring_indicator()?;
+        lines.rts = self.read_request_to_send().unwrap_or(false);
+        lines.dtr = self.read_data_terminal_ready().unwrap_or(false);
+        Ok(lines)
+    }
+}
+
+
+
+impl<T,G> AdvancedSerialPort for SerialPortWithGpios<T,G>
 where
     T: serialport::SerialPort,
     G: GpioOut + Send,
@@ -301,10 +267,12 @@ where
     }
     fn can_set_carrier_detect(&self) -> bool {
         self.cd_out_gpio.is_some()
-    }   
+    }
+
+    
 
     fn read_request_to_send(&mut self) -> serialport::Result<bool> {
-        match self.last_set_dtr {
+        match self.last_set_rts {
             Some(level) => Ok(level),
             None => {
                 Err(serialport::Error::new(
@@ -315,7 +283,7 @@ where
         }   
     }   
     fn read_data_terminal_ready(&mut self) -> serialport::Result<bool> {
-        match self.last_set_rts {
+        match self.last_set_dtr {
             Some(level) => Ok(level),
             None => {
                 Err(serialport::Error::new(
@@ -329,12 +297,12 @@ where
     fn set_ring_indicator(&mut self, level: bool) -> serialport::Result<()> {
         if let Some(pin) = self.ri_out_gpio.as_mut() {
             pin.set_value(
-                if (level) {
+                if level {
                     GpioValue::High 
                 } else {
                     GpioValue::Low
                 }
-            ).or_else(|e| {
+            ).or_else(|_e| {
                 Err(serialport::Error::new(
                     serialport::ErrorKind::Unknown,
                     format!("Failed to set Ring Indicator"),
@@ -357,7 +325,7 @@ where
                 } else {
                     GpioValue::Low
                 }
-            ).or_else(|e| {
+            ).or_else(|_e| {
                 Err(serialport::Error::new(
                     serialport::ErrorKind::Unknown,
                     format!("Failed to set Carrier Detect")
